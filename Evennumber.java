5debef28ec8a4c8cb7dd63b55302810d17e62fe1
Prathima-Sri <153510461+Prathima-Sri@users.noreply.github.com>
2023-12-15 09:45:36 +0530

    Create Evennumber.java

---
+++ Evennumber.java
@@ -0,0 +1,9 @@
+public class Evennumber {
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 1 to 28:");
+
+        for (int i = 2; i <= 28; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
